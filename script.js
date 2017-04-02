7e26f83b2fb510edf844333b5e2ac12032cd870d
jackhou147 <jackhou147@gmail.com>
2017-04-02 15:32:20 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -14,6 +14,13 @@ $(document).ready(function(){
     var $portfolioLink = $(".nav-page__portfolio-link");
     var $aboutLink = $(".nav-page__about-link");
     var $contactLink = $(".nav-page__contact-link");
+    var $navPage = $(".nav-page");
+    var $navPageLinks = $(".nav-page__links");
+    var $navPageFooter=$(".nav-page__footer");
+    var $firstOutline = $(".nav-page__links .outline:nth-child(1)");
+    var $secondOutline = $(".nav-page__links .outline:nth-child(2)");
+    var $thirdOutline = $(".nav-page__links .outline:nth-child(3)");
+    var $fourthOutline = $(".nav-page__links .outline:nth-child(4)");
     var allPages = [$(".home"),$(".portfolio"),$(".about"),$(".contact")];
     var allNavLinks = [$homeLink,$portfolioLink,$aboutLink,$contactLink];
     var currentPage; //string of current page name
@@ -22,23 +29,15 @@ $(document).ready(function(){
     }
     function removeOutLine(element){
         element.css("border-color","transparent");
-    }
+    };
     function updateCurrentPage(){
         allPages.forEach(function(page){
             if(!page.hasClass("none-display")){
             currentPage = page.attr("class");
             }
         })
-    }
-    
-    
-    /***************NAVBAR*************************/
-    var $navBar = $(".nav-bar");
-    var $navBtn = $(".nav-bar__btn");
-    var $navLogo = $(".nav-bar__logo");
-    var $navBtnClicked = false;
-    $navBtn.click(function(){
-        function animateNavBtn(){
+    };
+    function animateNavBtn(){
             var $line1 = $(".nav-bar__btn hr:nth-child(1)");
             var $line2 = $(".nav-bar__btn hr:nth-child(2)");
             var $line3 = $(".nav-bar__btn hr:nth-child(3)");
@@ -75,13 +74,56 @@ $(document).ready(function(){
                 })
             };
             $navBtnClicked ? reverse() : animate();
+            $navBtnClicked = !$navBtnClicked;
+        
         };
-        function showNavPage(){
-            $navPage.toggleClass("none-display");
-        };
-        function disableScrolling(){
-            $("body").toggleClass("none-scroll");
-        };
+    function animateNavPage(){
+            /*$navPage.fadeToggle(250);*/
+            if($navBtnClicked){
+                $navPage.fadeIn();
+                setTimeout(function(){
+                    tl.play();
+                },400);
+            }else {
+                tl.reverse();
+                setTimeout(function(){
+                    $navPage.fadeOut();
+                },1000)
+                /*$navPage.delay(1500).fadeOut();*/
+                
+            }
+        }
+    /***************NAVBAR*************************/
+    var $navBar = $(".nav-bar");
+    var $navBtn = $(".nav-bar__btn");
+    var $navLogo = $(".nav-bar__logo");
+    var $navBtnClicked = false;
+    var tl = new TimelineLite();
+    tl.from($firstOutline,0.5,{
+    y:30,
+    autoAlpha:0,
+    ease: Bounce.easeOut
+    })
+    .from($secondOutline,0.5,{
+    y:30,
+    autoAlpha:0,
+    ease: Bounce.easeOut
+    },"-=0.3")
+    .from($thirdOutline,0.5,{
+    y:30,
+    autoAlpha:0,
+    ease: Bounce.easeOut
+    },"-=0.3")
+    .from($fourthOutline,0.5,{
+    y:30,
+    autoAlpha:0,
+    ease: Bounce.easeOut
+    },"-=0.3");
+    tl.reversed(true);
+    function toggle(){
+        tl.reversed() ? tl.play():tl.reverse();
+    }
+    $navBtn.click(function(){
         function highlightCurrentPage(){
             updateCurrentPage();
             allNavLinks.forEach(function(link){
@@ -94,28 +136,32 @@ $(document).ready(function(){
                 }
             })
         };
+        function disableScrolling(){
+            $("body").toggleClass("none-scroll");
+        };
+        /*animateNavBtn();*/
+        $(this).clearQueue();
         animateNavBtn();
-        showNavPage();
+        animateNavPage();
         highlightCurrentPage();
         disableScrolling();
-        $navBtnClicked = !$navBtnClicked;
     })
     
     /****************NAVPAGE************************/
-    var $navPage = $(".nav-page");
+    
     allNavLinks.forEach(function(link){
         var thisPage = link.attr("class").replace("nav-page__","").replace("-link","");
         link.parent().click(function(){
-            function closeNavPage(){
-                $navPage.addClass("none-display");
-            };
             function enableScrolling(){
                 $("body").removeClass("none-scroll");
             };
-            closeNavPage();
-            toThisPage(thisPage);
-            updateCurrentPage();
-            enableScrolling();
+            animateNavBtn(); 
+            animateNavPage();
+            setTimeout(function(){
+                toThisPage(thisPage);
+                updateCurrentPage();
+                enableScrolling();
+            },1300);
         });
         link.parent().hover(function(){
             setOutline(link.parent());
